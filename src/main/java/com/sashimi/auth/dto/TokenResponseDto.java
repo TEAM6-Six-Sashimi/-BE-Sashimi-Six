dca1cb70022e846b1b56335e0ca484bf2a18daf7
minseo0327 <barkmin9@naver.com>
2026-05-27 21:48:16 +0900

    #103 로그인 성공 시 회원 이름 반환 추가

--- src/main/java/com/sashimi/auth/dto/TokenResponseDto.java
+++ src/main/java/com/sashimi/auth/dto/TokenResponseDto.java
@@ -11,4 +11,15 @@ public class TokenResponseDto {
     private String accessToken;
     private String refreshToken;
     private Long accessTokenExpiresIn;
+    private String name;
+
+    public TokenResponseDto withName(String name) {
+        return TokenResponseDto.builder()
+                .grantType(this.grantType)
+                .accessToken(this.accessToken)
+                .refreshToken(this.refreshToken)
+                .accessTokenExpiresIn(this.accessTokenExpiresIn)
+                .name(name)
+                .build();
+    }
 }
