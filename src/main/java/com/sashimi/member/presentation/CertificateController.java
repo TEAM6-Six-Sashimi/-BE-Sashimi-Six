2a6b64f6c75279946e4d82420cffb29b71aac0c9
yuji-jeoung <489q8188@g.eulji.ac.kr>
2026-05-28 00:33:58 +0900

    #100 [FEATURE] 사용자 자격증 목록 조회 API 구현

--- src/main/java/com/sashimi/member/presentation/CertificateController.java
+++ src/main/java/com/sashimi/member/presentation/CertificateController.java
@@ -3,6 +3,7 @@ package com.sashimi.member.presentation;
 import com.sashimi.member.application.command.DeleteCertificateCommand;
 import com.sashimi.member.application.command.RegisterCertificateCommand;
 import com.sashimi.member.application.usecase.CertificateCommandUseCase;
+import com.sashimi.member.application.usecase.CertificateQueryUseCase;
 import com.sashimi.member.presentation.api.response.ApiResponse;
 import com.sashimi.member.presentation.api.response.CertificateResponse;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +12,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
 public class CertificateController {
 
     private final CertificateCommandUseCase certificateCommandUseCase;
+    private final CertificateQueryUseCase certificateQueryUseCase;
 
     @PostMapping(value = "/{userId}/certificates", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse<CertificateResponse>> registerCertificate(
@@ -42,5 +46,13 @@ public class CertificateController {
                 new DeleteCertificateCommand(userId, certificationId)
         );
         return ResponseEntity.ok(ApiResponse.of("자격증이 삭제되었습니다."));
+    }
+
+    @GetMapping("/{userId}/certificates")
+    public ResponseEntity<ApiResponse<List<CertificateResponse>>> getCertificates(
+            @PathVariable Long userId
+    ) {
+        List<CertificateResponse> responses = certificateQueryUseCase.getCertificates(userId);
+        return ResponseEntity.ok(ApiResponse.of("자격증 목록 조회 성공", responses));
     }
 }
