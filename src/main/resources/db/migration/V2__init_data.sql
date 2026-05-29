0d2fdcc56c0bb3f149af77cb7f9c8c9d393d4cbc
minseo0327 <barkmin9@naver.com>
2026-05-29 14:43:54 +0900

    # 146 [UPDATE] 프로필 분리 개선 + db 더미데이터 삽입

--- src/main/resources/db/migration/V2__init_data.sql
+++ src/main/resources/db/migration/V2__init_data.sql
@@ -1,12 +1,12 @@
 INSERT IGNORE INTO users
 (login_id, email, password, name, birth_date, phone, role, status, email_verified, referral_code)
 VALUES
-    ('admin01', 'admin@test.com', '$2a$10$admin', '관리자', '1985-01-01', '010-0000-0000', 'ADMIN', 'ACTIVE', TRUE, 'ADMIN001'),
+    ('admin01', 'admin@test.com', '$2a$10$HFStfIJEmYPf7MIpzQ4aSeqGSN.IWC4ZCUKHxKy7E/m4/xxWD1f4C', '관리자', '1985-01-01', '010-0000-0000', 'ADMIN', 'ACTIVE', TRUE, 'ADMIN001'),
     ('instructor01', 'instructor1@test.com', '$2a$10$SwKCvl1vtkjOSh0F.KflsOx8eolg1MTZdqUXcfzZQ4piogm8.PosK', '김강사', '1988-03-12', '010-1111-1111', 'INSTRUCTOR', 'ACTIVE', TRUE, 'INS001'),
-    ('instructor02', 'instructor2@test.com', '$2a$10$instructor', '이강사', '1990-07-20', '010-2222-2222', 'INSTRUCTOR', 'ACTIVE', TRUE, 'INS002'),
+    ('instructor02', 'instructor2@test.com', '$2a$10$SwKCvl1vtkjOSh0F.KflsOx8eolg1MTZdqUXcfzZQ4piogm8.PosK', '이강사', '1990-07-20', '010-2222-2222', 'INSTRUCTOR', 'ACTIVE', TRUE, 'INS002'),
     ('student01', 'student1@test.com', '$2a$10$vYwWcwkU/lHpqmL3WERfgOi0yx/OxCkfbtGQHJjIj8IpfTdGptORi', '박학생', '2001-05-14', '010-3333-3333', 'STUDENT', 'ACTIVE', TRUE, 'STD001'),
-    ('student02', 'student2@test.com', '$2a$10$student', '최학생', '2000-10-03', '010-4444-4444', 'STUDENT', 'ACTIVE', TRUE, 'STD002'),
-    ('student03', 'student3@test.com', '$2a$10$student', '정학생', '1999-12-25', '010-5555-5555', 'STUDENT', 'ACTIVE', TRUE, 'STD003');
+    ('student02', 'student2@test.com', '$2a$10$vYwWcwkU/lHpqmL3WERfgOi0yx/OxCkfbtGQHJjIj8IpfTdGptORi', '최학생', '2000-10-03', '010-4444-4444', 'STUDENT', 'ACTIVE', TRUE, 'STD002'),
+    ('student03', 'student3@test.com', '$2a$10$vYwWcwkU/lHpqmL3WERfgOi0yx/OxCkfbtGQHJjIj8IpfTdGptORi', '정학생', '1999-12-25', '010-5555-5555', 'STUDENT', 'ACTIVE', TRUE, 'STD003');
 
 INSERT IGNORE INTO categories
 (main_category_id, name, sub_category, sort_order, is_active)
