fc562c11d2b8286dd085ec719cbc8b02ded08cf9
yuji-jeoung <489q8188@g.eulji.ac.kr>
2026-05-30 10:31:35 +0900

    #157 [BUGFIX] 권한 수정, 강사 신청 목록용 추가

--- src/main/java/com/sashimi/member/application/usecase/MemberQueryUseCase.java
+++ src/main/java/com/sashimi/member/application/usecase/MemberQueryUseCase.java
@@ -1,10 +1,13 @@
 package com.sashimi.member.application.usecase;
 
-import com.sashimi.member.presentation.api.response.InstructorApplicationResponse;
+import com.sashimi.member.presentation.api.response.InstructorApplicationDetailResponse;
+import com.sashimi.member.presentation.api.response.InstructorApplicationListResponse;
 
 import java.util.List;
 
 public interface MemberQueryUseCase {
 
-    List<InstructorApplicationResponse> getPendingInstructorApplications();
+    List<InstructorApplicationListResponse> getPendingInstructorApplications();
+
+    InstructorApplicationDetailResponse getInstructorApplicationDetail(Long applicationId);
 }
