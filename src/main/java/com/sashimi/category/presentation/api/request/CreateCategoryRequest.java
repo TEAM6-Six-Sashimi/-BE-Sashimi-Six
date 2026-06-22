9c8e1e389e5626c6cbaf0ab710522e68b5878b21
pjm0202 <pjm1234p@gmail.com>
2026-06-22 15:47:13 +0900

    카테고리 CRUD 코드래빗 수정

--- src/main/java/com/sashimi/category/presentation/api/request/CreateCategoryRequest.java
+++ src/main/java/com/sashimi/category/presentation/api/request/CreateCategoryRequest.java
@@ -1,6 +1,8 @@
 package com.sashimi.category.presentation.api.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateCategoryRequest(
-        String name,
-        String subCategory
+        @NotBlank(message = "카테고리명은 필수입니다.") String name,
+        @NotBlank(message = "세부 카테고리명은 필수입니다.") String subCategory
 ) {}
