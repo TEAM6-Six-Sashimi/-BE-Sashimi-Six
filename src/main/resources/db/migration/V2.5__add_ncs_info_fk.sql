3a9a40a7ab3c842d5a2d636132b3327efd6a9be6
030clin <030clin@gmail.com>
2026-06-24 10:56:41 +0900

    [UPDATE] #290 Flyway V2 마이그레이션 통합 및 경로 분리

--- src/main/resources/db/migration/V2.5__add_ncs_info_fk.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE courses
-    ADD CONSTRAINT fk_courses_ncs_info
-        FOREIGN KEY (ncs_info_id) REFERENCES ncs_info(ncs_info_id)
-            ON DELETE SET NULL
-            ON UPDATE CASCADE;
