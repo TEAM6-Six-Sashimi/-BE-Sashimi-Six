1db8b60f8b73ce940bc854d822b5f4c8943ea173
minseo0327 <barkmin9@naver.com>
2026-06-30 17:02:05 +0900

    [UPDATE] 사이트 권한 설정

--- src/main/java/com/sashimi/security/config/SecurityConfig.java
+++ src/main/java/com/sashimi/security/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
                 "http://3.35.52.52",
                 "http://www.sixsashimi.com",
                 "https://www.sixsashimi.com",
-                "https://sixsashimi.com"
+                "https://sixsashimi.com",
+                "https://www.sixsashimi.com.market-app.org"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of(
