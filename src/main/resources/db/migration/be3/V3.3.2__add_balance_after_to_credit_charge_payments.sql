f5530048c2e6dd99f2ee34853ca9db4ef1fd6e00
030clin <030clin@gmail.com>
2026-07-13 20:00:55 +0900

    [UPDATE] DB 방식 변경

--- src/main/resources/db/migration/be3/V3.3.2__add_balance_after_to_credit_charge_payments.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE credit_charge_payments
-    ADD COLUMN balance_after BIGINT NULL;
--- src/main/resources/db/migration/be5/V3.5.2__fix_instructor_profiles_category.sql
+++
@@ -1,5 +0,0 @@
-UPDATE instructor_profiles SET category_id = 1 WHERE instructor_profile_id = 1;
-UPDATE instructor_profiles SET category_id = 7 WHERE instructor_profile_id = 2;
-UPDATE instructor_profiles SET category_id = 3 WHERE instructor_profile_id = 3;
-UPDATE instructor_profiles SET category_id = 8 WHERE instructor_profile_id = 4;
-UPDATE instructor_profiles SET category_id = 2 WHERE instructor_profile_id = 5;
