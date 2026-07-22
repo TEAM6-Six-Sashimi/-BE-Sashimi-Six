0a98a3fae210d043536bee82c5080924d309b95d
yuji-jeoung <489q8188@g.eulji.ac.kr>
2026-07-22 16:55:26 +0900

    [UPDATE] 수강평 신고 목록 조회 N+1 쿼리 개선

--- src/main/java/com/sashimi/review/infrastructure/persistence/SpringDataReviewRepository.java
+++ src/main/java/com/sashimi/review/infrastructure/persistence/SpringDataReviewRepository.java
@@ -2,7 +2,11 @@ package com.sashimi.review.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SpringDataReviewRepository extends JpaRepository<ReviewJpaEntity, Long> {
 
     boolean existsByUserIdAndCourseId(Long userId, Long courseId);
+
+    List<ReviewJpaEntity> findAllByIdIn(List<Long> ids);
 }
